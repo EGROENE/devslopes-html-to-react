40353f14e0bc3d02c3b78fe5c10070021189da76
Ethan <ethangroene@gmail.com>
2023-08-09 15:55:48 -0400

    Add top characters

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,8 +3,17 @@ import "./header.css";
 import "./character-cards.css";
 import "./character-ratings.css";
 import "./reset.css";
-
+import { data } from "../../public/fma-data";
+console.log(data);
 function App() {
+  let topCharacters = data
+    .sort(function (a, b) {
+      return b.votes - a.votes;
+    })
+    .slice(0, 5);
+  const isIndexMultipleOfTwo = (character) => {
+    return (topCharacters.indexOf(character) + 1) % 2 === 0 ? true : false;
+  };
   return (
     <>
       <header>
@@ -18,11 +27,23 @@ function App() {
       <section id="character-ratings">
         <h4>Top Characters</h4>
         <table>
-          <tr>
-            <th>Name</th>
-            <th>Skillset</th>
-            <th>Votes</th>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Skillset</th>
+              <th>Votes</th>
+            </tr>
+            {topCharacters.map((character) => (
+              <tr
+                key={character.name}
+                className={isIndexMultipleOfTwo(character) ? "light" : "dark"}
+              >
+                <td>{character.name}</td>
+                <td>{character.skillset.join(", ")}</td>
+                <td>{character.votes}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </section>
     </>
