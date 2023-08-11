736047d24a09ab1287cf875975f114463b0922d1
Ethan <ethangroene@gmail.com>
2023-08-11 12:59:47 -0400

    Make, implement component for page header

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import "./App.css";
+import PageHeader from "./Components/PageHeader/PageHeader";
 import { data } from "../public/fma-data";
 import CharacterRatings from "../src/Components/CharacterRatings/CharacterRatings";
 import CharacterCards from "../src/Components//CharacterCards/CharacterCards";
-import "./header.css";
 import "./reset.css";
 function App() {
   return (
     <>
-      <header>
-        <h1>Fullmetal Alchemist</h1>
-        <nav>
-          <li>about us</li>
-          <li>info</li>
-          <li>support us</li>
-        </nav>
-      </header>
+      <PageHeader />
       <CharacterRatings characterData={data} />
       <CharacterCards characterData={data} />
     </>
